92d6b0aa2707af26985d6e1e153d205359c561fb
ninoorta <54931407+ninoorta@users.noreply.github.com>
2019-10-28 16:06:13 +0700

    Add files via upload

---
+++ CodeArtist.js
@@ -0,0 +1,154 @@
+
+function getInformation1() {
+
+    let singerName = document.getElementById("1").innerText;
+    document.getElementById("1").innerHTML = `<img src="https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/avatars/e/e/ee58fcc0ff45002b8d416bd7685809ce_1487040461.jpg" width ="auto"; height = "auto" `
+        +
+        `<div id ="tieu-su">
+
+    <div>Sơn Tùng M-TP (Tên thật là Nguyễn Thanh Tùng)<div> 
+    Sinh ra và lớn lên tại một vùng quê của tỉnh Thái Bình. Vốn sở hữu “gen di truyền” từ người mẹ của mình, một nghệ sĩ biểu diễn hát chèo tại Nhà hát Thái Bình nên Tùng đã bộc lộ khả năng âm nhạc của mình ngay từ khi còn là một cậu bé mới chập chững những bước đi đầu tiên.
+
+Năm 2009, anh cùng với các bạn cùng lớp đã thành lập một nhóm nhạc lấy tên là Over Band, bắt đầu sáng tác và đăng tải các bài hát của mình lên một trang web độc lập chuyên về lĩnh vực âm nhạc.
+
+Năm 2011, Tùng cũng tham gia một nhóm nhạc có tên là Young Pilots biểu diễn khắp các sân khấu của tỉnh nhà và gặp hái được không ít những thành công. Trong khoảng thời gian này, Tùng đã lấy nghệ danh là M-TP, có nghĩa là “music” (âm nhạc), “tài năng” và “phong cách”, một sự đan xen khá thú vị giữa tiếng Việt và tiếng Anh.
+
+Tháng 8-2011, Sơn Tùng thu âm bài hát “Cơn mưa ngang qua” và nhận được những thành công ngoài mong đợi, bài hát sau đó liên tiếp “đại náo” các bảng xếp hạng lớn nhỏ tại Việt Nam và đứng ngang hàng với các tên tuổi nổi tiếng lúc bấy giờ.
+
+Cũng chính từ đó, cái tên Sơn Tùng M-TP bắt đầu được nhắc đến nhiều hơn trong làng giải trí Việt. Không chỉ thành công trên con đường âm nhạc, Sơn Tùng còn “lấn sân” sang cả lĩnh vực điện ảnh với vai diễn nam chính trong bộ phim điện ảnh “Chàng trai năm ấy” của đạo diễn Nguyễn Quang Dũng.
+
+<div>Năm 2012, Sơn Tùng quyết định thi tuyển vào Nhạc viện TPHCM, một trong những ngôi trường có tỉ lệ chọi vô cùng “khốc liệt”.Mặc dù giọng hát của Sơn Tùng không có một chút kỹ thuật nào nhưng chỉ với một tháng luyện thi chăm chỉ, quên cả việc ăn ngủ, Sơn Tùng đã đỗ thủ khoa Nhạc viện TPHCM chuyên ngành Thanh nhạc với số điểm 25,5.
+
+Ít năm sau, những sản phẩm âm nhạc liên tiếp được chàng trai gốc Thái Bình tung ra thị trường như “Em của ngày hôm qua”, “Chạy ngay đi”, “Lạc trôi”,… đã trở thành những sản phẩm đình đám nhất của thị trường âm nhạc Việt Nam, ngoài ra những sản phẩm này cũng lan tỏa sang cả một số quốc gia khác trên thế giới.
+
+MV “Hãy trao cho anh” của Sơn Tùng M-TP đã phá vỡ mọi kỷ lục trên bảng xếp hạng Youtube tại Việt Nam cũng như châu lục và toàn thế giới. “Hãy trao cho anh” đã tự thiết lập nên những kỷ lục lịch sử của làng nhạc Việt: đạt 1 triệu views chỉ trong 8 phút, 2 triệu views trong 14 phút, 5 triệu views trong 1 giờ, 10 triệu views trong 3 giờ và tính đến thời điểm hiện tại, MV “Hãy trao cho anh” đã cán mốc 12 triệu lượt xem trong vòng chưa đến 18 giờ. MV được xem nhiều nhất thế giới trong ngày 1/7, đồng thời đứng thứ 14 trên top trending YouTube toàn cầu. Tính riêng ở khu vực châu lục, Sơn Tùng nghiễm nhiên sở hữu MV được xem nhiều nhất Châu Á trong ngày 1/7.
+    </div>
+    </div>
+
+    <div ><strong><br/>20 bài hát trending của <strong>${singerName}:</strong></div>
+    <div id ="list-videos" style="display: inline-block;"></div>
+     `
+    getTrendingVideos(singerName);
+
+}
+
+
+
+
+function getInformation2() {
+
+    let singerName = document.getElementById("2").innerText;
+    document.getElementById("2").innerHTML = `<img src="https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/avatars/8/b/4/c/8b4c040a3f0a64672e8dad7078aff3d1.jpg" width ="auto"; height = "auto" `
+        +
+        `<div id ="tieu-su">
+        Tên thật: Nguyễn Hoàng Sơn   <div>Ngày sinh: 10/09/1992</div>   <div>Quốc Gia: Việt Nam</div>
+
+        Là một ca sĩ, nhạc sĩ và rapper trẻ đang được nhiều người yêu mến và Soobin hiện là ca sĩ của SpaceSpeakers Music Productionz. Tuy Hoàng Sơn sinh ra và lớn lên ở Hà Nội nhưng anh lại tập trung phát triển sự nghiệp ca hát của mình hơn trong những năm gần đây tại Tp. HCM.
+        Sở trường của Soobin Hoàng Sơn là các ca khúc R&B, thế nhưng mới đây nam ca sĩ Hà Thành này lại khiến khán giả không khỏi bất ngờ khi phát hành ca khúc mang phong cách pop ballad có tựa đề "Phía sau một cô gái", một sáng tác của nữ ca nhạc sĩ Tiên Cookie - Đánh dấu một nét nổi bật mới dành cho nam ca sĩ này. Không chỉ đổi mới về phong cách âm nhạc, Soobin Hoàng Sơn còn trau chuốt về hình ảnh lẫn giọng hát trong sản phẩm âm nhạc mới này. Bài hát này còn là một trong những bài hát được nghe nhiều nhất tại Việt Nam trong năm 2016. Tính đến ngày 10 tháng 10 năm 2017, bản Audio chính thức đã thu hút được 238,971,037 lượt nghe trên Zing Mp3.
+    </div>
+
+    <div ><strong><br/>20 bài hát trending của <strong>${singerName}:</strong></div>
+    <div id ="list-videos" style="display: inline-block;"></div>
+     `
+    getTrendingVideos(singerName);
+
+}
+
+
+
+
+
+
+function getInformation3() {
+
+    let singerName = document.getElementById("3").innerText;
+    document.getElementById("3").innerHTML = `<img src="https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/avatars/0/2/9/a/029a1e473c230215ee5d272eb89cd571.jpg" width ="auto"; height = "auto" `
+        +
+        `<div id ="tieu-su">
+
+        <div>Tên thật: Phan Mạnh Quỳnh</div> <div>Ngày sinh: 10/01/1990</div> <div>Quốc Gia: Việt Nam</div>
+        Phan Mạnh Quỳnh sinh ra và lớn lên ở xóm Đồng Hà, xã Diễn Vạn, huyện Diễn Châu, tỉnh Nghệ An. 
+        Từ nhỏ, Phan Mạnh Quỳnh có đam mê sáng tác nhạc. Năm 2009, Phan Mạnh Quỳnh đậu Trường Đại học Công nghiệp Thành phố Hồ Chí Minh và vào Thành phố Hồ Chí Minh để học tập. Phan Mạnh Quỳnh là tác giả của nhiều bản hit mà ca sĩ Ưng Hoàng Phúc, Khởi My, Hồ Quang Hiếu, Miu Lê... từng thể hiện. Không chỉ sáng tác, anh còn được cộng đồng mạng yêu mến khi tự thể hiện các ca khúc của mình với chất giọng lạ, trầm ấm và gần gũi với cuộc sống như "Người yêu cũ", "Nước ngoài", "Bước qua thế giới", "Mất hy vọng", "Khi phải quên đi"... Tháng 9 năm 2015, Phan Mạnh Quỳnh phát hành album Vợ người ta và thành công lớn với ca khúc chủ đề cùng tên đã trở thành một hiện tượng trong năm 2015. Ca khúc "Vợ người ta" đã lập kỷ lục đạt 6 triệu lượt nghe chỉ sau 2 tuần phát hành (chỉ tính riêng trên một trang nhạc trực tuyến), đứng Top 5 cho ba hạng mục giải thưởng của Zing Music Awards: Ca khúc của năm, Album của năm và Music Video của năm. 
+        Tính đến tháng 12 năm 2015, "Vợ người ta" đã cán mốc 100 triệu lượt nghe và 32 triệu lượt xem trên kênh âm nhạc trực tuyến. Ca khúc Vợ người ta được ca sĩ trình bày lại trong tập 7, chương trình Hội ngộ danh hài năm 2016. Năm 2016, Phan Mạnh Quỳnh đã sáng tác và biểu diễn bài hát mới "Hồi Ức" trong chương trình Sing My Song (Bài Hát Hay Nhất) Tập 7. Với bài hát Vợ người ta đã giúp Phan Mạnh Quỳnh nhận các giải thưởng: -Tại lễ trao giải Zing Music Awards 2015. -Ca khúc Soul/R&B được yêu thích nhất. -Ca khúc được chia sẻ nhiều nhất cộng đồng mạng. 
+        Tại chương trình Vietnam Top Hits 2015: -Top năm ca khúc của năm. -Hiện tượng âm nhạc 2015.
+        
+ 
+    </div>
+
+    <div ><strong><br/>20 bài hát trending của <strong>${singerName}:</strong></div>
+    <div id ="list-videos" style="display: inline-block;"></div>
+     `
+    getTrendingVideos(singerName);
+
+}
+
+
+
+
+function getInformation4() {
+
+    let singerName = document.getElementById("4").innerText;
+    document.getElementById("4").innerHTML = `<img src="https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/avatars/4/8/9/1/4891c7eef87e3ac85a50a2fba2674f5a.jpg" width ="auto"; height = "auto" `
+        +
+        `<div id ="tieu-su">
+        <div>Tên thật: Lê Nguyễn Trung Đan</div>    <div>Ngày sinh: 24/05/1988</div>    <div>Quốc Gia: Việt Nam</div>
+        Tham gia cộng đồng underground Việt từ năm 2008, Binz đã từng là artist của một số diễn đàn rap như Midsiderap, GVR. Được biết tới rộng rãi hơn trong năm 2010 với mixtape ”M-da Legend” & những sản phẩm đáng chú ý kết hợp với rapper Cá Chép của Vietdreamerz, người nghe rap nhớ tới Binz là một artist trẻ miền Trung có giọng hát ngọt ngào, kĩ thuật tốt và luôn mới mẻ. Các album của anh: Nếu Như Là Định Mệnh - Bin , Hoài Lâm Sau Mỗi Đêm Dài - JC Hưng , Binz (2017) Cơn Mưa Cuối (Single) Hoa Hồng Dại - Binz , KanDy , Ken Tamz Tết Này Con Về - Isaac Thái , Binz Chỉ Còn Mình Anh (Single) (2016) Tát Nước Đầu Đình (Single) Điều Bí Mật (Single)-ItsLee , Binz 
+        Những Bài Hát Hay Nhất Của Binz (2015) Bâng Khuâng (Rap Version) (Single) - JustaTee , Binz (2014) Crying Over You (Single) JustaTee , Binz (2013) Nếu Như Là Định Mệnh - Hoài Lâm ft. Binz (2017) HOA HỒNG DẠI - Binz(2017)
+    </div>
+
+    <div ><strong><br/>20 bài hát trending của <strong>${singerName}:</strong></div>
+    <div id ="list-videos" style="display: inline-block;"></div>
+     `
+    getTrendingVideos(singerName);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getTrendingVideos(name) {
+
+    let xhr = new XMLHttpRequest();
+    let keyword = '20 bài hát trending của' + name + ' Official Music Video';
+
+    let url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=" +
+        keyword + "&type=video&key=AIzaSyA0EXoz8yj9TA8GzYy_60N8umR-c2I6PI4"
+
+    xhr.open("GET", url);
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            getVideo(this.responseText);
+        }
+    }
+
+    xhr.send();
+}
+
+function getVideo(json) {
+    let video = JSON.parse(json);
+    let ds = video.items;
+    let videoIDs = [];
+
+    for (let i = 0; i < ds.length; i++) {
+        videoIDs.push(ds[i].id.videoId)
+    }
+
+    for (let i = 0; i < videoIDs.length; i++) {
+        document.getElementById("list-videos").innerHTML +=
+
+            ` <iframe title='YouTube video player' width='400' height='300' src="https://www.youtube.com/embed/` + videoIDs[i] + `"` +
+            `frameborder='0' allowFullScreen> </iframe> `
+
+    }
+
+
+}
